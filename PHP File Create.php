eaf993b84ac04d4e5fd69f97c097a2f8b9e44bbe
wenka25 <wenkasalinding04@gmail.com>
2025-04-25 20:11:37 +0800

    coba PHP

---
+++ PHP File Create.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+    $txt = "Mickey Mouse\n";
+    fwrite($myfile, $txt);
+    $txt = "Minnie Mouse\n";
+    fwrite($myfile, $txt);
+    fclose($myfile);
+    ?>
+
+    <?php
+    $myfile = fopen("newfile.txt", "a") or die("Unable to open file!");
+    $txt = "Donald Duck\n";
+    fwrite($myfile, $txt);
+    $txt = "Goofy Goof\n";
+    fwrite($myfile, $txt);
+    fclose($myfile);
+    ?>
+
+</body>
+
+</html>
